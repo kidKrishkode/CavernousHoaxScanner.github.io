32fbe4a78390ef5002fb4208722d797a5c1cc5de
Krishnendu Mitra <150600460+kidKrishkode@users.noreply.github.com>
2024-08-05 22:00:18 +0530

    Version 1.0

--- config/security.ts
+++ config/security.ts
@@ -53,5 +53,48 @@ module.exports = {
             str = str.replaceAll(antihash[i][1], antihash[i][0]);
         }
         return str.toString();
+    },
+    browser: function browser(navigator){ 
+        var browserAgent = navigator['user-agent']; 
+        var browserName, browserVersion, browserMajorVersion; 
+        var Offset, OffsetVersion, ix; 
+        if((OffsetVersion = browserAgent.indexOf("Chrome")) != -1){ 
+            browserName = "Chrome"; 
+            browserVersion = browserAgent.substring(OffsetVersion + 7);
+        }else if((OffsetVersion = browserAgent.indexOf("MSIE")) != -1){ 
+            browserName = "Microsoft Internet Explorer"; 
+            browserVersion = browserAgent.substring(OffsetVersion + 5); 
+        }else if((OffsetVersion = browserAgent.indexOf("Firefox")) != -1){ 
+            browserName = "Firefox"; 
+        }else if((OffsetVersion = browserAgent.indexOf("Safari")) != -1){ 
+            browserName = "Safari"; 
+            browserVersion = browserAgent.substring(OffsetVersion + 7); 
+            if((OffsetVersion = browserAgent.indexOf("Version")) != -1) 
+                browserVersion = browserAgent.substring(OffsetVersion + 8); 
+        }else if((Offset = browserAgent.lastIndexOf(' ') + 1) < (OffsetVersion = browserAgent.lastIndexOf('/'))){ 
+            browserName = browserAgent.substring(Offset, OffsetVersion); 
+            browserVersion = browserAgent.substring(OffsetVersion + 1); 
+            if(browserName.toLowerCase() == browserName.toUpperCase()){ 
+                browserName = navigator.appName; 
+            } 
+        } 
+        if((ix = browserVersion.indexOf(";")) != -1) 
+            browserVersion = browserVersion.substring(0, ix); 
+        if((ix = browserVersion.indexOf(" ")) != -1) 
+            browserVersion = browserVersion.substring(0, ix); 
+            browserMajorVersion = parseInt('' + browserVersion, 10); 
+        if(isNaN(browserMajorVersion)){ 
+            browserVersion = '' + parseFloat(navigator.appVersion); 
+            browserMajorVersion = parseInt(navigator.appVersion, 10); 
+        }
+        return [browserName,browserVersion];
+    },
+    validBrowser: function validBrowser(browser_info,list){
+        for(let i=0; i<list.length; i++){
+            if(list[i].name == browser_info[0] && browser_info[1] >= list[i].version){
+                return true;
+            }
+        }
+        return false;
     }
 };
