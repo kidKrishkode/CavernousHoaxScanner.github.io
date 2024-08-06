ea9da8608e429e7d7753c2d975ff293a02fe0869
Krishnendu Mitra <150600460+kidKrishkode@users.noreply.github.com>
2024-08-06 21:18:30 +0530

    Version 1.0

--- model/imgConverter.py
+++ model/imgConverter.py
@@ -1,24 +1,57 @@
 import sys
 import cv2
+import numpy as np
 import json
 import ast
 
 def convert_png(image_path):
-    image = cv2.imread(image_path)
-    filename, extension = image_path.rsplit('.', 1)
-    new_image_path = f"{filename}.png"
-    cv2.imwrite(new_image_path, image, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+    image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+    if image.shape[2] == 4:
+        filename, extension = image_path.rsplit('.', 1)
+        new_image_path = f"{filename}.png"
+        cv2.imwrite(new_image_path, image, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+    else:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2BGRA)
+        alpha_channel = np.ones((image.shape[0], image.shape[1]), dtype=image.dtype) * 255
+        image = np.dstack((image, alpha_channel))
+        filename, extension = image_path.rsplit('.', 1)
+        new_image_path = f"{filename}.png"
+        cv2.imwrite(new_image_path, image, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
     return new_image_path
 
 def convert_jpg(image_path):
-    image = cv2.imread(image_path)
+    image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+    if image.shape[2] == 4:
+        image = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
+    elif image.shape[2] == 1:
+        image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
     filename, extension = image_path.rsplit('.', 1)
     new_image_path = f"{filename}.jpg"
+    cv2.imwrite(new_image_path, image, [int(cv2.IMWRITE_JPEG_QUALITY), 95])
+    return new_image_path
+
+def convert_jpeg(image_path):
+    image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+    if image.shape[2] == 4:
+        image = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
+    elif image.shape[2] == 1:
+        image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+    filename, extension = image_path.rsplit('.', 1)
+    new_image_path = f"{filename}.jpeg"
     cv2.imwrite(new_image_path, image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
     return new_image_path
 
+def convert_gif(image_path):
+    return image_path
+
+def convert_bmp(image_path):
+    return image_path
+
+def convert_svg(image_path):
+    return image_path
+
 def is_image(image_path):
-    valid_extensions = ['.jpg', '.jpeg', '.png', '.bmp', '.gif']
+    valid_extensions = ['.jpg', '.jpeg', '.png', '.bmp', '.gif', '.svg']
     if not any(image_path.endswith(ext) for ext in valid_extensions):
         return False
 
@@ -46,6 +79,14 @@ def convert_image(input_list):
             new_image_path = convert_png(image_path)
         elif extension == 'jpg':
             new_image_path = convert_jpg(image_path)
+        elif extension == 'jpeg':
+            new_image_path = convert_jpeg(image_path)
+        elif extension == 'gif':
+            new_image_path = convert_gif(image_path)
+        elif extension == 'bmp':
+            new_image_path = convert_bmp(image_path)
+        elif extension == 'svg':
+            new_image_path = convert_svg(image_path)
         else:
             print("Unwanted media convertion try.")
         return new_image_path
