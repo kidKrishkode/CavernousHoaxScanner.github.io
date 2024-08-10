334acbe3bf1e476e46623d806654eeee94ae31a4
Krishnendu Mitra <111632830+Krishnendu-Mitra@users.noreply.github.com>
2024-08-10 11:25:14 +0530

    Version 1.0
    Extension var update

--- model/Preprocessor.py
+++ model/Preprocessor.py
@@ -5,6 +5,7 @@ import base64
 import logging
 
 img_extensions = ['.jpg', '.jpeg', '.png', '.peng', '.bmp', '.gif', '.webp', '.svg', '.jpe', '.jfif', '.tar', '.tiff', '.tga']
+vdo_extensions = ['.mp4','.mov', '.wmv', '.avi', '.avchd', '.flv', '.f4v', '.swf', '.mkv', '.webm', '.html5']
 
 def data_url_to_cv_image(data_url):
     data_url = data_url.split(',')[1]
@@ -16,3 +17,16 @@ def data_url_to_cv_image(data_url):
 def json_log(message):
     logging.basicConfig(filename='json_data.log',level=logging.DEBUG)
     logging.debug(json.dumps({"result": message}))
+
+def is_image(image_path):
+    valid_extensions = img_extensions
+    if not any(image_path.endswith(ext) for ext in valid_extensions):
+        return False
+    try:
+        img = cv2.imread(image_path)
+        if img is not None:
+            return True
+    except Exception as e:
+        print(f"Error to reading image: {e}")
+
+    return False
