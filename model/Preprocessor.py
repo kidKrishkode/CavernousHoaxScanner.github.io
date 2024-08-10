751ece941ed94d36a47a98d6a6f4bb11b53e6d94
Krishnendu Mitra <111632830+Krishnendu-Mitra@users.noreply.github.com>
2024-08-10 11:13:29 +0530

    Version 1.0
    Extension var update

--- model/Preprocessor.py
+++ model/Preprocessor.py
@@ -4,6 +4,8 @@ import numpy as np
 import base64
 import logging
 
+img_extensions = ['.jpg', '.jpeg', '.png', '.peng', '.bmp', '.gif', '.webp', '.svg', '.jpe', '.jfif', '.tar', '.tiff']
+
 def data_url_to_cv_image(data_url):
     data_url = data_url.split(',')[1]
     img_bytes = base64.b64decode(data_url)
