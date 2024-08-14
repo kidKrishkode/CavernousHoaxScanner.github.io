62525712d3e10d0c8b88fff3e36555816559a721
Krishnendu Mitra <111632830+Krishnendu-Mitra@users.noreply.github.com>
2024-08-14 22:59:43 +0530

    Version 1.0

--- config/env-variables.ts
+++ config/env-variables.ts
@@ -54,5 +54,19 @@ module.exports = {
             name: "Safari",
             version: 80
         },
-    ]
+    ],
+    error_templet: `
+        <div class="workspace blbg" style="background: #0000009e;" id="errorPreview">
+            <div class="errorView">
+                <header class="flx"><img src="../public/favicon.png" alt="load"/>
+                    <span style="cursor: pointer;" onclick="system.closePyError();">&times;</span>
+                </header>
+                <div class="error-message">
+                    <i class="fa fa-times-circle-o"></i>
+                    <h2>Error: <|error.code|>!</h2>
+                    <small class="form-text text-muted">This error genaredted by system for handle unwanted useage of resource</small>
+                    <p><|error.message|></p>
+                <div class="btn btn-process" style="margin-top: 40px;" onclick="system.closePyError();"><i class="fa fa-refresh"></i> Re-try</div>
+            </div>
+        </div>`
 };
---
+++ config/hex.ts
@@ -0,0 +1,18 @@
+module.exports = {
+    pyerrorscanner: function pyerrorscanner(data){
+        if((data*1) - (data*1) == 0){
+            return true;
+        }else{
+            return false;
+        }
+    },
+    pyerrorinfo: function pyerrorinfo(error_log, code){
+        let message = '';
+        for(let i=0; i<error_log.length; i++){
+            if(error_log[i].code == code){
+                message = error_log[i].desc;
+            }
+        }
+        return {code, message};
+    }
+};
