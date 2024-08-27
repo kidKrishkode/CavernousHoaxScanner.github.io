1b7c23294cae52b8af6d5b1c683c459f7b91441b
Krishnendu Mitra <111632830+Krishnendu-Mitra@users.noreply.github.com>
2024-08-27 21:02:26 +0530

    Version 1.0

--- model/imgToPdf.py
+++ model/imgToPdf.py
@@ -4,6 +4,20 @@ import cv2
 from fpdf import FPDF
 import Preprocessor
 
+def collage_pdf(image_paths):
+    output_path = "./assets/pdfhouse/temp.pdf"
+    pdf = FPDF()
+    for image_path in image_paths:
+        page_width, page_height = 210, 297
+        new_width = (page_width / 2 ) - 6
+        new_height = (len(image_paths) / 2) - 4
+        pdf.add_page(orientation= 'P' if  new_width <= new_height else 'L')
+        x_offset = (page_width - new_width) / 2 if new_width < page_width else 0
+        y_offset = (page_height - new_height) / 2 if new_height < page_height else 0
+        pdf.image(image_path, x_offset, y_offset, new_width, new_height)
+    pdf.output(output_path)
+    return output_path
+
 def create_pdf(image_paths):
     output_path = "./assets/pdfhouse/temp.pdf"
     pdf = FPDF()
