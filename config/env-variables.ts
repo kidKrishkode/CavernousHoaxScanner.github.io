702f0bea209cb9199b8a32578198d21a959352bc
Krishnendu Mitra <111632830+Krishnendu-Mitra@users.noreply.github.com>
2025-05-24 18:37:11 +0530

    Version 1.0

--- config/env-variables.ts
+++ config/env-variables.ts
@@ -67,14 +67,14 @@ module.exports = {
         <div class="workspace blbg" style="background: #0000009e;" id="errorPreview">
             <div class="errorView">
                 <header class="flx"><img src="../public/favicon.ico" alt="load"/>
-                    <span style="cursor: pointer;" onclick="new TAB().closeAll(); system.closePyError();">&times;</span>
+                    <span style="cursor: pointer;" onclick="system.closePyError();">&times;</span>
                 </header>
                 <div class="error-message">
                     <i class="fa fa-times-circle-o"></i>
                     <h2>Error: <|error.code|>!</h2>
                     <small class="form-text text-muted">This error genaredted by system for handle unwanted useage of resource</small>
                     <p><|error.message|></p>
-                <div class="btn btn-process" style="margin-top: 40px;" onclick="new TAB().closeAll(); system.closePyError();"><i class="fa fa-refresh"></i> Re-try</div>
+                <div class="btn btn-process" style="margin-top: 40px;" onclick="system.closePyError();"><i class="fa fa-refresh"></i> Re-try</div>
             </div>
         </div>`,
     traffic: {
