625a2443d9513475674ea75f7ac0bc2d576e9b8c
Krishnendu Mitra <111632830+Krishnendu-Mitra@users.noreply.github.com>
2025-06-11 21:10:31 +0530

    Version 1.0

--- public/App.js
+++ public/App.js
@@ -618,6 +618,30 @@ System.prototype.getInterImage = async function(){
         alert('Please fillout the url field for Image fetching, Empty field is not acceptable!');
     }
 }
+System.prototype.validateApiKey = function(id1, id2){
+    const apiKey = document.getElementById(id1).value;
+    const messageElement = document.getElementById(id2);
+    const regex = /^[a-z0-9]{32}$/;
+    if (regex.test(apiKey)) {
+        messageElement.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                                        <path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"/>
+                                    </svg><br>Your API key is configured in the right manner, You are Good to go`;
+        messageElement.style.color = "#28a745";
+    } else {
+        messageElement.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+                                    </svg><br>Please provide your actual API Key for code integration`;
+        messageElement.style.color = "#dc3545";
+    }
+}
+System.prototype.validapiKey = function(id){
+    const apiKey = document.getElementById(id).value;
+    const regex = /^[a-z0-9]{32}$/;
+    return regex.test(apiKey);
+}
+
 System.prototype.sendMemory = function(){
     fetch('/memory', {
         method: 'POST',
